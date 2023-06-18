72fc247ffffb3d9a844f125214d12f5e637aaa18
Icung <icung@Icungs-MacBook-Air.local>
2023-06-19 00:52:24 +0700

    Add a Category View

---
+++ Landmarks/Categories/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Icung on 19/06/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
